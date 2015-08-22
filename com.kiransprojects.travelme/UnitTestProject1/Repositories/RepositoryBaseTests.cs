2ce01c3db276bab5d78ac95bd9a0cd9f5003b5f8
Kiran <kiran_patel94@hotmail.com>
2015-08-22 20:31:48 +0100

    added data access test project and moved references for frameworks

---
+++ com.kiransprojects.travelme/UnitTestProject1/Repositories/RepositoryBaseTests.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTestProject1.Repositories
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    
+    public class RepositoryBaseTests
+    {
+    }
+}
