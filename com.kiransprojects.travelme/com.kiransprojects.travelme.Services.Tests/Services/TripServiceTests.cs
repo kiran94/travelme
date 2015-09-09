c32352f45eb1b2c6d0fa8bcd56017815a05f71d1
Kiran <kiran_patel94@hotmail.com>
2015-09-09 06:00:52 +0100

    Developed TripService furthur with tests

--- com.kiransprojects.travelme/com.kiransprojects.travelme.Services.Tests/Services/TripServiceTests.cs
+++ com.kiransprojects.travelme/com.kiransprojects.travelme.Services.Tests/Services/TripServiceTests.cs
@@ -39,5 +39,77 @@
             Assert.AreEqual(trips, list); 
         }
 
+        /// <summary>
+        /// Ensures a null is returned when the trip does not exist
+        /// </summary>
+        [Test]
+        public void GetTrips_NonExisting_Null()
+        {
+            Mock<IRepository<Trip>> Repository = new Mock<IRepository<Trip>>();
+            Mock<ITripRepository> TripRepository = new Mock<ITripRepository>();
+            TripRepository.SetupSequence(o => o.GetTrips(It.IsAny<Guid>())).Returns(null); 
+
+            TripService Service = new TripService(Repository.Object, TripRepository.Object);
+            IList<Trip> list = Service.GetTrips(Guid.NewGuid());
+            Assert.IsNull(list); 
+        }
+
+        /// <summary>
+        /// Ensrures trips can be added
+        /// </summary>
+        [Test]
+        public void AddTrip_NonExistingEntity_Added()
+        {
+            Mock<IRepository<Trip>> Repository = new Mock<IRepository<Trip>>();
+            Mock<ITripRepository> TripRepository = new Mock<ITripRepository>();
+            Repository.Setup(o => o.Insert(It.IsAny<Trip>())); 
+
+            TripService Service = new TripService(Repository.Object, TripRepository.Object);
+            Trip trip = new Trip()
+            {
+                ID = Guid.NewGuid()
+            };
+            bool flag = Service.AddTrip(new Trip());
+            Assert.IsTrue(flag);
+        }
+
+        /// <summary>
+        /// Ensures trips can be added
+        /// </summary>
+        [Test]
+        public void AddTrip_ExistingEntity_NotAdded()
+        {
+            Mock<IRepository<Trip>> Repository = new Mock<IRepository<Trip>>();
+            Mock<ITripRepository> TripRepository = new Mock<ITripRepository>();
+            Repository.Setup(o => o.Insert(It.IsAny<Trip>())).Throws<Exception>();
+
+            TripService Service = new TripService(Repository.Object, TripRepository.Object);
+            Trip trip = new Trip()
+            {
+                ID = Guid.NewGuid()
+            };
+            bool flag = Service.AddTrip(new Trip());
+            Assert.IsFalse(flag); 
+        }
+
+        /// <summary>
+        /// Ensures trips can be updated
+        /// </summary>
+        [Test]
+        public void EditTrip_ExistingEntity_Updated()
+        {
+            Mock<IRepository<Trip>> Repository = new Mock<IRepository<Trip>>();
+            Mock<ITripRepository> TripRepository = new Mock<ITripRepository>();
+            Repository.Setup(o => o.Update(It.IsAny<Trip>(), true));
+
+            TripService Service = new TripService(Repository.Object, TripRepository.Object);
+            Trip trip = new Trip()
+            {
+                ID = Guid.NewGuid()
+            };
+
+            Trip Retrieved = Service.EditTrip(trip);
+            Assert.AreEqual(trip, Retrieved); 
+        }
     }
 }
--- com.kiransprojects.travelme/com.kiransprojects.travelme.Services/Services/TripService.cs
+++ com.kiransprojects.travelme/com.kiransprojects.travelme.Services/Services/TripService.cs
@@ -17,6 +17,9 @@
         /// </summary>
         private readonly IRepository<Trip> _repository = null;
 
+        /// <summary>
+        /// Trip Repository Methods
+        /// </summary>
         private readonly ITripRepository _tripRepository = null; 
 
         /// <summary>
@@ -49,19 +52,76 @@
             return list; 
         }
 
+        /// <summary>
+        /// Passes a trip to the data access layer to persist
+        /// </summary>
+        /// <param name="trip">Trip to save</param>
+        /// <returns>flag indicating if operation was successful</returns>
         public bool AddTrip(Trip trip)
         {
-            throw new NotImplementedException();
+            if(trip == null)
+            {
+                return false; 
+            }
+
+            try
+            {
+                this._repository.Insert(trip);
+                return true; 
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return false; 
+            }
         }
 
-        public Framework.Entities.Trip EditTrip(Trip trip)
+        /// <summary>
+        /// Passes an existing trip to update
+        /// </summary>
+        /// <param name="trip"></param>
+        /// <returns>updated trip</returns>
+        public Trip EditTrip(Trip trip)
         {
-            throw new NotImplementedException();
+            if (trip == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                this._repository.Update(trip, true);
+                return trip; 
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return null;
+            }
         }
 
+        /// <summary>
+        /// Deletes a trip
+        /// </summary>
+        /// <param name="trip">trip to delete</param>
+        /// <returns>flag indicating if trip was successfull</returns>
         public bool DeleteTrip(Trip trip)
         {
-            throw new NotImplementedException();
+            if (trip == null)
+            {
+                return false;
+            }
+
+            try
+            {
+                this._repository.Delete(trip);
+                return true; 
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return false; 
+            }
         }
 
         public IList<Location> GetLocations(Guid ID)
