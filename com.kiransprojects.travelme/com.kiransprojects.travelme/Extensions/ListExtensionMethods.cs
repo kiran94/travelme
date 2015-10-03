0a4f55379c9ef5e9d092fe29c87860b7eba8eb1d
Kiran <kiran_patel94@hotmail.com>
2015-10-03 10:53:59 +0100

    Trip add and list partials and worked on view, added extension methods for List types

---
+++ com.kiransprojects.travelme/com.kiransprojects.travelme/Extensions/ListExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿namespace com.kiransprojects.travelme.Extensions
+{
+    using com.kiransprojects.travelme.Framework.Entities;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// List Extension Methods
+    /// </summary>
+    public static class ListExtensionMethods
+    {
+        /// <summary>
+        /// Checks if a list is ready to be displayed
+        /// </summary>
+        /// <typeparam name="T">Type of List</typeparam>
+        /// <param name="value">List to be searched</param>
+        /// <returns>Flag indicating if the list is not empty, not null and has content</returns>
+        public static bool HasContent<T>(this List<T> value) where T : EntityBase
+        {
+            if (value == null || value.Count == 0)
+            {
+                return false; 
+            }
+
+            for (int i = 0; i < value.Count; i++)
+            {
+                if (value[i] != null)
+                {
+                    return true; 
+                }
+            }
+
+            return false; 
+        }
+    }
+}
